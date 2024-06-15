cdfdb57701abe0493a03e7ec5cce781dc2ac57dd
AyeMyatMyatKhine <ayemyatmyatkhine@metateammyanmar.com>
2024-06-15 14:23:18 +0900

    first commit

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button :type="type" @click="handleClick" class="button">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent ({
+    
+});
+</script>
